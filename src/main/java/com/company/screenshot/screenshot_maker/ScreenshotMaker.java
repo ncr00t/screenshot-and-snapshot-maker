69dc7fbd281c4a76b910d69d08cc2f9abd3cbd3e
ncr00t <geritix@mail.ru>
2019-01-13 08:55:27 +0300

    deleted method for save screenshots in home directory because method should be in other class

--- src/main/java/com/company/screenshot/screenshot_maker/ScreenshotMaker.java
+++ src/main/java/com/company/screenshot/screenshot_maker/ScreenshotMaker.java
@@ -1,34 +1,13 @@
 package com.company.screenshot.screenshot_maker;
 
-import com.company.screenshot.file_manager.FileManager;
-import com.company.screenshot.name_generator.ScreenshotNameGenerator;
 import com.github.sarxos.webcam.Webcam;
-
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class ScreenshotMaker {
-    private FileManager fileManager;
-    private ScreenshotNameGenerator screenshotNameGenerator;
 
     public ScreenshotMaker() {
 
-    }
-
-    public String saveScreenshotInHomeDirectory(ScreenshotNameGenerator screenshotNameGenerator) throws AWTException, IOException {
-        BufferedImage screenshot = makeScreenshotFromScreen();
-        String screenshotNameWithExtension = screenshotNameGenerator.generateScreenshotNameWithDate();
-
-        fileManager = new FileManager();
-        File homeDirectory = fileManager.getHomeDirectory();
-        File homeDirectoryWithScreenshot = fileManager.getHomeDirectoryWithScreenshot(screenshotNameWithExtension);
-
-        ImageIO.write(screenshot, screenshotNameGenerator.getExtension(),
-                                    homeDirectoryWithScreenshot);
-        return homeDirectory + "/" + screenshotNameWithExtension;
     }
 
     public BufferedImage makeScreenshotFromScreen() throws AWTException {
